981c23cf59ed844b66c8810c92e9b615d0893b29
Andrew Del Prete <andrew@musicbed.com>
2017-11-25 20:32:59 -0900

    Add initial example w/ glamorous js

---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { css } from "glamor";
+import glamorous from "glamorous";
+
+const Heading = glamorous.h1(tw("text-red md:text-green"));
+
+function App({}) {
+  return <Heading>Heading</Heading>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ example/webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+
+module.exports = {
+  entry: "./src/App.js",
+  output: {
+    path: __dirname + "./dist",
+    filename: "app.bundle.js"
+  },
+  module: {
+    loaders: [{ test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ }]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
